bce7795d2701f2b008623c878f95e8efb6da4fe4
HP <HP@DESKTOP-KKJRIED>
2019-02-04 21:49:25 +0530

    Here updating a String Reverse Program

---
+++ java/src/javaPrograms/StringReverse.java
@@ -0,0 +1,16 @@
+package javaPrograms;
+
+public class StringReverse {
+	public static void main(String[] args) {
+		String s1="mohan";
+		String s2= " ";
+
+		for (int i =s1.length()-1;i>=0; i--) {
+			char c= s1.charAt(i);
+			s2=s2+c;
+			
+		}
+		System.out.println("the string is "+s2);
+	}
+
+}
